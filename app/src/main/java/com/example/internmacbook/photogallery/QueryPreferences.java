2009f352268526e6f500ba640bcef712907a7677
Intern MacBook3 <internmacbook@Tonic-2011-spare-pro.local>
2016-08-02 14:26:45 -0400

    post chapter 25, added Flickr search functionality

---
+++ app/src/main/java/com/example/internmacbook/photogallery/QueryPreferences.java
@@ -0,0 +1,23 @@
+package com.example.internmacbook.photogallery;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by internmacbook on 8/2/16.
+ */
+public class QueryPreferences {
+    private static final String PREF_SEARCH_QUERY = "searchQuery";
+
+    public static String getStoredQuery(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(PREF_SEARCH_QUERY, null);
+    }
+
+    public static void setStoredQuery(Context context, String query) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(PREF_SEARCH_QUERY, query)
+                .apply();
+    }
+}
