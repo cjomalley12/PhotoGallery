f564236544aa929d1f1bd46f0e7c5d4fc9e34b39
Intern MacBook3 <internmacbook@Tonic-2011-spare-pro.local>
2016-08-03 09:34:35 -0400

    post chapter 26, added background service and made notification

--- app/src/main/java/com/example/internmacbook/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/example/internmacbook/photogallery/PhotoGalleryFragment.java
@@ -1,5 +1,6 @@
 package com.example.internmacbook.photogallery;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -113,13 +114,20 @@ public class PhotoGalleryFragment extends Fragment {
         });
 
         searchView.setOnSearchClickListener(new View.OnClickListener(){
-
             @Override
             public void onClick(View v) {
                 String query = QueryPreferences.getStoredQuery(getActivity());
                 searchView.setQuery(query, false);
             }
         });
+
+        MenuItem toggleItem = menu.findItem(R.id.menu_item_toggle_polling);
+        if(PollService.isServiceAlarmOn(getActivity())) {
+            toggleItem.setTitle(R.string.stop_polling);
+        }
+        else {
+            toggleItem.setTitle(R.string.start_polling);
+        }
     }
 
     @Override
@@ -128,6 +136,11 @@ public class PhotoGalleryFragment extends Fragment {
             case R.id.menu_item_clear:
                 QueryPreferences.setStoredQuery(getActivity(), null);
                 updateItems();
+                return true;
+            case R.id.menu_item_toggle_polling:
+                boolean shouldStartAlarm = !PollService.isServiceAlarmOn(getActivity());
+                PollService.setServiceAlarm(getActivity(), shouldStartAlarm);
+                getActivity().invalidateOptionsMenu();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
